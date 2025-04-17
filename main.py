b4f4541826f330c6b06444607c24a6c06272cc7f
Nathan Harris <nharrisanalyst@gmail.com>
2025-04-17 15:40:42 -0700

    added login,logout,register routes

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import sqlalchemy
 from .routes import login
 from models.main_db import User
 from auth.UserManager import get_user_manager
+from auth.Auth_Backend import auth_backend
+from .schemas import UserCreate, UserRead
 
 fastapi_users = FastAPIUsers[User, uuid.UUID](
     get_user_manager,
@@ -31,3 +33,15 @@ async def read_hello():
 async def read_alchemy_version():
     return{'alchemy-version':sqlalchemy.__version__}
 
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
